249c969e06138c72b158a3c2c57c53f7bf792323
Grant Willoughby <94477072+Gwill10@users.noreply.github.com>
2024-04-21 18:27:32 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ECE484_App
+//
+//  Created by Makenzie Dietz on 4/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var count = 0 // State variable to track button presses
+
+    var body: some View {
+        VStack {
+            Spacer() // Spacer to push the text to the top
+            Text("Grant Willoughby  ECE484 Lab") // Text above the button
+                .font(.title)
+                .padding()
+            Spacer() // Spacer to push the text to the top
+
+            Button(action: {
+                self.count += 1 // Increment count when button is pressed
+            }) {
+                Text("Press me!") // Button text
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .cornerRadius(10)
+            }
+
+            Text("Count: \(count)") // Display count below the button
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ ECE484_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECE484_AppApp.swift
+//  ECE484_App
+//
+//  Created by Makenzie Dietz on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ECE484_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
